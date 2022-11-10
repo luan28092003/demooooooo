cf1d70a4013b70cbc3172c880693ac7db9b3669c
phamdat23 <112074117+phamdat23@users.noreply.github.com>
2022-11-10 15:02:42 +0700

    tao code2

--- app/src/main/java/luanntph26376/fpoly/myapplication/MainActivity.java
+++ app/src/main/java/luanntph26376/fpoly/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /// tao cmt
         getActionBar();
+        // sàdsafsdafsad
     }
 }
