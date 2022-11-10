5af22fd5829b800a0315fa79f8b2fc85adb6a71e
tienDat203 <datntph26471@fpt.edu.vn>
2022-11-10 15:31:58 +0700

    commit by Tien Dat

---
+++ app/src/main/java/luanntph26376/fpoly/myapplication/TienDatActivity.java
@@ -0,0 +1,14 @@
+package luanntph26376.fpoly.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TienDatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tien_dat);
+    }
+}
